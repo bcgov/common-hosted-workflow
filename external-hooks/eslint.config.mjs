73ce18460f68cd6358d27b718db388fd67a4cb27
j.dev <junmin.dev@outlook.com>
2026-05-11 11:13:39 -0700

    chore(CCP-4496): upgrade external-hooks directory dependencies

--- external-hooks/eslint.config.mjs
+++ external-hooks/eslint.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'no-undef': 'off',
     },
