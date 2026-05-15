1f2b2baeece595d348c26b44fe8cfab278dae233
Junmin Ahn <36021827+junminahn@users.noreply.github.com>
2026-05-15 08:59:28 -0700

    refactor(CCP-4487): rename DevXConnectorApi to DevXConnector (#183)

--- community-nodes/credentials/DevXConnector.credentials.ts
+++ community-nodes/credentials/DevXConnector.credentials.ts
@@ -1,7 +1,7 @@
 import { Icon, ICredentialType, INodeProperties } from 'n8n-workflow';
 
-export class DevXConnectorApi implements ICredentialType {
-  name = 'devXConnectorApi';
+export class DevXConnector implements ICredentialType {
+  name = 'devXConnector';
   icon: Icon = 'file:../icons/message-2-code.svg';
   displayName = 'DevX Message Connector API';
   documentationUrl = 'https://github.com/bcgov/common-hosted-workflow';
